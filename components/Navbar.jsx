1387ede5bb495d275692c80d8029feca5628c047
krisztian kaposi <krisztiankaposi@hotmail.com>
2023-04-26 19:07:39 +0100

    first

---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+const Navbar=()=> {
+    return (
+        <div>
+        <div>
+        <Link/>
+
+        <h1>KJ Painting</h1>
+        <ul>
+            <li></li>
+        </ul>
+        </div>
+        </div>
+    )
+}
+export default Navbar
