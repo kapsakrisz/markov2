d1336d8012140f8b4ba80c62e01744bed8ca01a3
krisztian kaposi <krisztiankaposi@hotmail.com>
2023-05-07 11:08:58 +0100

    contact done

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-const Contact = () => {
+import {useForm,SubmitHandler} from "react-hook-form";
+
+
+
+
+
+const Contact =()=> {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data =>{
+    window.location.href = `mailto:devkapsakrisz@gmail.com?subject=${data.subject}&body=Hi my name is ${data.name}. ${data.message} (${data.email})`;
+  }
+
+
+
   return (
     <div className='max-w-[1240px] m-auto p-4 h-screen'>
         <h1 className='text-2xl font-bold text-center p-4'>Lets work together</h1>
-        <form className='max-w-[600px] m-auto'>
+        <form onSubmit={handleSubmit(onSubmit)} className='max-w-[600px] m-auto'>
             <div className='grid grid-cols-2 gap-2'>
-                <input className='border shadow-lg p-3' type="text" placeholder='Name' />
-                <input className='border shadow-lg p-3' type="email" placeholder='Email' />
+                <input {...register('name')} className='border shadow-lg p-3' type="text" placeholder='Name' />
+                <input {...register('email')} className='border shadow-lg p-3' type="email" placeholder='Email' />
             </div>
-            <input className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Subject' />
-            <textarea className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Message'></textarea>
+            <input {...register('subject')} className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Subject' />
+            <textarea {...register('message')} className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Message'></textarea>
             <button className='border shadow-lg p-3 w-full mt-2'>Submit</button>
         </form>
     </div>
--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -5,7 +5,7 @@ import Hero from '../components/Hero'
 const contact = () => {
   return (
     <div>
-        <Hero heading='Contact' message='Submit the form below please' />
+        <Hero heading='Contact' message='Submit the form below please or email me at  kj_painting_decorating@yahoo.com' />
         <Contact />
     </div>
   )
